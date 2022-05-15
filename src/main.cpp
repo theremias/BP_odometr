b3ea457bebe6d8a09097b629aaf3d1e23ca4d970
theremias <104557989+theremias@users.noreply.github.com>
2022-05-15 03:54:24 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,391 @@
+#include "common.h"
+
+/**
+ * @brief inicialization of bluetooth module 
+ */
+SoftwareSerial bluetooth { PIN_BLUETOOTH_RX, PIN_BLUETOOTH_TX, false };
+
+
+/**
+ * @brief reading on distance encoder
+ */
+volatile int dist_position = 0;
+
+/**
+ * @brief reading on direction encoder
+ * 
+ */
+volatile int dirc_position = 0;
+
+
+void ISR_distance() {
+    if ( digitalRead( dist_PinB ) == LOW ) {
+      dist_position--;
+    } else {
+      dist_position++;
+    }
+    if (dist_position % 10 == 0) {
+          DEBUGG("Ctena 10. zmena na vzdalenostnim encoderu");
+    }
+}
+
+void ISR_direction() {
+    if ( digitalRead( dirc_PinB ) == LOW ) {
+      dirc_position--;
+    } else {
+      dirc_position++;
+    }
+    if (dirc_position % 10 == 0) {
+      DEBUGG("Ctena 10. zmena na smerovem encoderu");
+    }
+}
+
+/**
+ * @brief offset of the direction encoder; which reading is corresponding to moving in straignt line
+ */
+int dirc_position_offset = 0;
+
+/**
+ * @brief number of differential computing since last enumeration of MP and Ref 
+ */
+int diffCount = 0;
+
+/**
+ * @brief time from estabilishing origin of current coordinate system
+ */
+int originMilisec = 0;
+
+// === === === DISTANCE ENCODER === === ===
+/**
+ * @brief distance traveled with one pulse of distance encoder
+ */
+float dist_step = M_PI * 2 * wheelRadius / dist_Ppr;
+
+/**
+ * @brief differential (center) angular distance between TPs 
+ */
+float phi;
+
+/**
+ * @brief angular distance of one pulse of direction encoder
+ */
+float dirc_step = 2 * M_PI / dirc_Ppr;
+
+/**
+ * @brief measured direction angle
+ */
+float theta;
+
+// === === === COORDINATE SYSTEM === === ===
+/**
+ * @brief coordinates of the TP of wheel
+ */
+coord_t XY_TP = {0, 0};
+
+/**
+ * @brief angle between +x axis and OLD TP to NEW TP line 
+ */
+float inclination_TP = 0;
+
+/**
+ * @brief XY coordinates of the NEW Ref position (while sending NMEA)
+ */
+coord_t XY_Ref = { hdist_TP_MP + parallel_MP_Ref, perpendicular_MP_Ref};
+
+/**
+ * @brief horizontal distance between MP and Ref
+ */
+float hdist_MP_Ref = sqrt( pow( parallel_MP_Ref, 2 ) + pow( perpendicular_MP_Ref, 2 ) );
+
+/**
+ * @brief angle between ateroposterior axis of vehicle and MP to Ref line 
+ */
+float kappa = atan2( perpendicular_MP_Ref, parallel_MP_Ref );
+
+/**
+ * @brief angle between +x axis and OLD MP to NEW MP line
+ */
+float inclination_MP;
+
+/**
+ * @brief horizontal distance between OLD MP and NEW MP
+ */
+float hdist_MP;
+
+/**
+ * @brief time when was the last message sent 
+ */
+float timeOfLastMessage = 0;
+
+void setup_encoders() {
+  pinMode (dist_PinA, INPUT_PULLUP);
+  pinMode (dist_PinB, INPUT_PULLUP);
+  pinMode (dirc_PinA, INPUT);
+  pinMode (dirc_PinB, INPUT);
+  DEBUGG("Encodery nastaveny...");
+  attachInterrupt(digitalPinToInterrupt(dist_PinA), ISR_distance, RISING);
+  attachInterrupt(digitalPinToInterrupt(dirc_PinA), ISR_direction, RISING);
+}
+
+polar_t compute_dist_inclin(coord_t XY_OLD, coord_t XY_NEW) {
+    polar_t result;
+    float dx = XY_NEW.X - XY_OLD.X;
+    float dy = XY_NEW.Y - XY_OLD.Y;
+    result.distance = sqrt( pow( dx, 2 ) + pow( dy, 2 ) );
+    result.inclination = atan2( dy, dx );
+    return result;
+}
+
+coord_t computeXY_polar(coord_t XY0, float distance, float inclination) {
+    coord_t newXY;
+    newXY.X = XY0.X + distance * cos( inclination );
+    newXY.Y = XY0.Y + distance * sin( inclination );
+    return newXY;
+}
+
+void compute_newTP(volatile int &dirc_reading, volatile int &dist_reading) {
+    theta = dirc_step * dirc_reading;
+    DEBUGG("Mereny uhel smeroveho enkoderu [rad]:"); DEBUGG(theta);
+    BDEBUGG("Mereny uhel smeroveho enkoderu [imp]:"); 
+    BDEBUGG(int(dirc_reading));
+
+    /**
+     * @brief differential horizontal distance between TPs
+     */
+    float diff_hdist_TP = dist_step * dist_reading;
+
+    DEBUGG("Diferencialni vzdalenost OLD TP a NEW TP [m]:"); DEBUGG(diff_hdist_TP);
+    
+    phi = 2.0 * asin( diff_hdist_TP * sin(theta) / ( 2.0 * ( hdist_axle_MP + hdist_TP_MP * cos(theta) ) ) );
+    DEBUGG("Uhlovy posun z OLD TP do NEW TP [rad]:"); DEBUGG(phi);
+
+    inclination_TP = inclination_TP + phi;
+    DEBUGG("Smernik z OLD TP na NEW TP [rad]:"); DEBUGG(inclination_TP);
+    XY_TP = computeXY_polar(XY_TP, diff_hdist_TP, inclination_TP);
+    DEBUGG("Souradnice NEW TP [X/m; Y/m]:");
+    DEBUGG(XY_TP.X); DEBUGG(XY_TP.Y);
+}
+
+polar_t oldRef2newRef() {
+    /**
+     * @brief angle between +x axis and TP to MP line 
+     */
+    float inclination_TP_MP = inclination_TP + phi / 2.0;
+    DEBUGG("Smernik z TP na MP:"); DEBUGG(inclination_TP_MP);
+
+    /**
+     * @brief coordinates of MP
+     */
+    coord_t XY_MP = computeXY_polar(XY_TP, hdist_TP_MP, inclination_TP_MP);
+    DEBUGG("Souradnice NEW MP [X/m; Y/m]:");
+    DEBUGG(XY_MP.X); DEBUGG(XY_MP.Y);
+
+    /**
+     * @brief coordinates of the old position of Ref; to compute distance and inclination
+     */
+    coord_t XY_Ref_Old = XY_Ref;
+
+    /**
+     * @brief angle between +x axis and MP to Ref line 
+     */
+    float inclination_MP_Ref = inclination_TP_MP + theta - kappa;
+    DEBUGG("Smernik z MP na Ref:"); DEBUGG(inclination_MP_Ref);
+
+    XY_Ref = computeXY_polar(XY_MP, hdist_MP_Ref, inclination_MP_Ref);
+    DEBUGG("Souradnice NEW Ref [X/m; Y/m]:");
+    DEBUGG(XY_Ref.X); DEBUGG(XY_Ref.Y);
+
+    polar_t disInc = compute_dist_inclin(XY_Ref_Old, XY_Ref);
+    DEBUGG("Smernik a delka z OLD Ref na NEW Ref [rad; m]:");
+    DEBUGG(disInc.inclination); DEBUGG(disInc.inclination);
+
+    return disInc;
+}
+
+void set_dircZero() {
+  /**
+   * @brief basically distance for which you should move in straight line
+   */
+  int count = 1000;
+  long int sum_dirc_reading = 0;
+  int old_dist_position = 0;
+  int reading_count = 0;
+  while (abs(dist_position) < count)
+  {
+    if( dist_position != old_dist_position ) {
+      sum_dirc_reading += dirc_position;
+      reading_count++;
+      old_dist_position = dist_position;
+      BDEBUGG("ujeta vzdalenost");
+      BDEBUGG(dist_position);
+      BDEBUGG("aktualni cteni");
+      BDEBUGG(dirc_position);
+      BDEBUGG("soucet cteni");
+      BDEBUGG(sum_dirc_reading);
+      BDEBUGG("cislo cteni");
+      BDEBUGG(reading_count);
+      }
+
+    if ( bluetooth.available() ) {
+    char BTinput = bluetooth.read(); 
+    if( BTinput == '0' ) {
+      char tiskni[]= "";
+      sprintf(tiskni, "%s %d", 
+                      "Cteni smeru: ",
+                      dirc_position );
+      bluetooth.println(tiskni);
+    }
+  }
+  }
+
+  dirc_position_offset = sum_dirc_reading / reading_count; 
+  dist_position = 0;
+  DEBUGG("Smer 0 nastaven"); 
+  DEBUGG( dirc_position_offset ); 
+  bluetooth.println("SMER 0 NASTAVEN\r\nUrcen offset:");
+  bluetooth.println(dirc_position_offset);
+}
+
+
+
+void doReset() {
+  // time of estabilishing origin of the system
+  originMilisec = millis();
+  // XY coordinates of the touch point of wheel
+  XY_TP = {0, 0};
+  // angle between +x axis and TP(n-1) to TP(n) line 
+  inclination_TP = 0;
+  // XY coordinates of the NEW MP position (while sending NMEA)
+  XY_Ref = {hdist_TP_MP + parallel_MP_Ref, perpendicular_MP_Ref};
+  dist_position = 0;
+  DEBUGG("Origin set.");
+  bluetooth.println("Definovan novy referencni souradny system.");
+}
+
+
+/**
+ * @brief Setup the Bluetooth object
+ * 
+ */
+void setup_Bluetooth() {
+  // zahájení komunikace s Bluetooth modulem
+  // skrze Softwarovou sériovou linku rychlostí 38400 baud
+  bluetooth.begin(38400);
+  bluetooth.println("Bluetooth spojeni navazano.");
+  DEBUGG("Arduino test, pri testu spojeni...");
+}
+
+/**
+ * @brief calculates checksum of a sentence (XOR)
+ * 
+ * @param NMEA_Sentence sentence to be calculated checksum for
+ * @param sentenceLength length of the sentence
+ * @return byte two hexadecimal characters
+ */
+byte calculateCheckSum(const char *NMEA_Sentence, int sentenceLength) {
+  byte result = 0;
+  for( int i = 0; i < sentenceLength; i++) {
+     result ^= NMEA_Sentence[i];
+  }
+  return result;
+}
+
+void create_NMEA(int originTime, float distance, float inclination) {
+  // computingCount = 0;
+  DEBUGG("Zacinam resit NMEA zpravu...");
+  char NMEA_inside[80] = "";
+  char NMEA_identificator[] = "SNODO";
+  float speed = distance / (millis() - timeOfLastMessage) * 1000.0;
+  timeOfLastMessage = millis();
+  int currentTime = (timeOfLastMessage - originTime) / 1000;
+  String NMEA_distance = String(distance * 1000, 1);
+  while (inclination < 0)   {
+    inclination += 2 * M_PI;
+  }
+  while (inclination > 2 * M_PI)   {
+    inclination -= 2 * M_PI;
+  }  
+  String NMEA_inclination = String(inclination, 4);
+  String NMEA_speed = String(speed, 2);
+  sprintf(NMEA_inside, "%s,%d,%s,%s,%s", 
+                        NMEA_identificator,
+                        currentTime, 
+                        NMEA_distance.c_str(),
+//                        distance >= 0 ? 'P' : 'N',
+                        NMEA_inclination.c_str(),
+                        NMEA_speed.c_str() );
+
+  byte NMEA_checksum = calculateCheckSum(NMEA_inside, sizeof(NMEA_inside));
+
+  char NMEA_whole[81];
+  sprintf(NMEA_whole, "$%s*%02hhx\r\n", NMEA_inside, NMEA_checksum);
+  DEBUGG("=== === === NMEA SENTENCE === === ==="); 
+  DEBUGG(NMEA_whole);
+  DEBUGG("^^^ ^^^ ^^^ NMEA SENTENCE ^^^ ^^^ ^^^");
+  bluetooth.print( NMEA_whole );
+
+}
+
+void makeMeasure() {
+    diffCount = 0;
+    polar_t toSend = oldRef2newRef();
+    create_NMEA(originMilisec, toSend.distance, toSend.inclination);
+}
+
+// sets the origin of reference coordinate system
+void checkForBluetooth() {
+  // if you want to reset the origin, send '0'
+  if ( bluetooth.available() ) {
+    char BTinput = bluetooth.read(); 
+    if( BTinput == '0' ) {
+      doReset();
+    } else if (BTinput == '1' ) {
+      DEBUGG("Jed metr rovne..."); 
+      bluetooth.println("NASTAVENI 0 SMEROVEHO ENCODERU\r\nJed rovne vpred...");
+      set_dircZero();
+    } else if (BTinput == '2') {
+      makeMeasure();
+      bluetooth.println("Domereno");
+    } else if (BTinput == '3') {
+      char tiskni[]= "";
+      sprintf(tiskni, "%s %d", 
+                      "Cteni smeru: ",
+                      dirc_position );
+      bluetooth.println(tiskni);
+    }
+  }
+}
+
+
+void setup() {
+  // initialize serial communication
+  // (115200 chosen because it is required for Teapot Demo output, but it's
+  // really up to you depending on your project)
+  Serial.begin(115200);
+  DEBUGG("Komunikace na serial portu zapocala...");
+  //setup_MPU();
+  //DEBUGG("MPU nastaveno");
+  setup_encoders();
+  setup_Bluetooth();
+}
+
+void loop() {
+ //read_MPU();
+  //DEBUGG("MPU precteno");
+  if ( abs( dist_position ) > dist_steps2compute) {
+    volatile int dirc_position_true = dirc_position - dirc_position_offset;
+    compute_newTP(dirc_position_true, dist_position);
+    dist_position = 0;
+    diffCount++;
+    DEBUGG("Diferencialni vypocet cislo:");
+    DEBUGG(diffCount);
+  }
+
+  if( diffCount >= compute2sendMultiplier) {
+    diffCount = 0;
+    polar_t toSend = oldRef2newRef();
+    create_NMEA(originMilisec, toSend.distance, toSend.inclination);
+  }
+  
+  checkForBluetooth();
+}
